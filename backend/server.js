6d0f4174bdd4a0cc6a848a7366d5134019e8e75c
varunkarthi1324 <vkvarunkarthikeyan@gmail.com>
2025-04-23 10:17:19 +0530

    add vercel config

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,9 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes); // Mount post routes
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend server!");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
